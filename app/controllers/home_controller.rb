4a3bd13bbeff57aa3dc7d0f904f0c33c9be4cfc0
Hans-Sebastian <hanssebastian.p@gmail.com>
2017-11-11 18:57:07 -0500

    download pdf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,13 @@
 class HomeController < ApplicationController
+
+	def index
+	end
+
+	def download_pdf
+	  send_file(
+	    "#{Rails.root}/public/Hans_Palacios_Resume.pdf",
+	    filename: "Hans_Palacios_Resume.pdf",
+	    type: "application/pdf"
+	  )
+	end
 end
